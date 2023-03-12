c609c4ea437a2c584a3432d61900c86419bb92ac
Lwandza Nzalo <lwandza.nzalo@gmail.com>
2023-03-12 21:07:31 +0200

    12 March 2023 21:07

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,5 +1,5 @@
 import { MoralisNextApi } from "@moralisweb3/next";
-const apiKeyFound = process.env.MORALIS_API_KEY,
+
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY!,
   authentication: {
