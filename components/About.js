00fe9f698e9d6bcc505ee421114bad2bcd968864
Lwandza Nzalo <lwandza.nzalo@gmail.com>
2023-03-12 21:35:59 +0200

    12 March 2023 21:35

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import AboutImg from "../assets/about.jpg"
+import AboutImg from "../assets/about.JPG"
 
 const About = () => {
   return (
